0995003caafa5c9354ea8ea01caf80b7563037ad
Elizabeth Nelson <ENelson@travisusd.org>
2018-09-14 11:09:23 -0700

    InventoryDbHelper and InventoryContract complete

---
+++ app/src/main/java/com/example/enelson/inventoryapptwo/data/InventoryContract.java
@@ -0,0 +1,61 @@
+package com.example.enelson.inventoryapptwo.data;
+
+import android.provider.BaseColumns;
+
+public final class InventoryContract {
+
+    private InventoryContract(){}
+
+    public static final class InventoryEntry implements BaseColumns {
+
+        /**Name of database table for inventory*/
+        public final static String TABLE_NAME = "inventory";
+
+        /**
+         * Unique ID for inventory
+         *
+         * Type: INTEGER
+         */
+        public final static String _ID = BaseColumns._ID;
+
+
+        /**
+         *Product Name
+         *
+         * Type: TEXT
+         */
+        public final static String COLUMN_PRODUCT_NAME = "name";
+
+        /**
+         * Price
+         *
+         * Type: INTEGER
+         */
+
+        public final static String COLUMN_PRODUCT_PRICE = "price";
+
+        /**
+         * Quantity
+         *
+         * Type: INTEGER
+         */
+
+        public final static String COLUMN_PRODUCT_QUANTITY = "quantity";
+
+        /**
+         * Supplier Name
+         *
+         * Type: INTEGER
+         */
+
+        public final static String COLUMN_PRODUCT_SUPPLIER_NAME = "supplier_name";
+
+        /**
+         * Supplier Phone
+         *
+         * Type: TEXT
+         */
+
+        public final static String COLUMN_PRODUCT_SUPPLIER_PHONE = "supplier_phone";
+    }
+}
---
+++ app/src/main/java/com/example/enelson/inventoryapptwo/data/InventoryDbHelper.java
@@ -0,0 +1,39 @@
+package com.example.enelson.inventoryapptwo.data;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.enelson.inventoryapptwo.data.InventoryContract.InventoryEntry;
+
+public class InventoryDbHelper extends SQLiteOpenHelper {
+
+    public static final String LOG_TAG = InventoryDbHelper.class.getSimpleName();
+
+    private static final String DATABASE_NAME = "inventory.db";
+
+    private static final int DATABASE_VERSION = 1;
+
+    public InventoryDbHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_INVENTORY_TABLE = "CREATE TABLE "
+                + InventoryEntry.TABLE_NAME + " ("
+                + InventoryEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + InventoryEntry.COLUMN_PRODUCT_NAME + " TEXT NOT NULL, "
+                + InventoryEntry.COLUMN_PRODUCT_PRICE + " INTEGER NOT NULL DEFAULT 0, "
+                + InventoryEntry.COLUMN_PRODUCT_QUANTITY + " INTEGER NOT NULL DEFAULT 0, "
+                + InventoryEntry.COLUMN_PRODUCT_SUPPLIER_NAME + " INTEGER, "
+                +InventoryEntry.COLUMN_PRODUCT_SUPPLIER_PHONE + " TEXT);";
+
+        db.execSQL(SQL_CREATE_INVENTORY_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+    }
+}
