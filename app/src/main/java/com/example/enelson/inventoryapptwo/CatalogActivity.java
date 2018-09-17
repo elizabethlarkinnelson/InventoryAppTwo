6f66a1c613640594ab146c3aaec888e1ba4dcfb3
Elizabeth Nelson <ENelson@travisusd.org>
2018-09-17 14:58:48 -0700

    activity_catalog.xml complete

--- app/src/main/java/com/example/enelson/inventoryapptwo/CatalogActivity.java
+++ app/src/main/java/com/example/enelson/inventoryapptwo/CatalogActivity.java
@@ -24,13 +24,11 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
     private static final int INVENTORY_LOADER = 0;
 
     InventoryCursorAdapter mCursorAdaptor;
-//    NEED TO IMPLEMENT ADAPTERVIEW!!!!!!!!!!
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
-//        NEED TO CREATIVE THIS LAYOUT!!!!!!
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener(){
