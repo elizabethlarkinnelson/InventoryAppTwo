b0cae7b30ab4df95d1a0684d5ff9469a1669f99f
Elizabeth Nelson <ENelson@travisusd.org>
2018-09-19 15:16:54 -0700

    Solved floating action button issue by updating dependencies in gradle file

--- app/src/main/java/com/example/enelson/inventoryapptwo/CatalogActivity.java
+++ app/src/main/java/com/example/enelson/inventoryapptwo/CatalogActivity.java
@@ -9,13 +9,13 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.support.design.widget.FloatingActionButton;
 
 import com.example.enelson.inventoryapptwo.data.InventoryContract;
 
